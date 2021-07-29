c8074041250cdcd54e1cde5629d82aa54d441230
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-29 10:37:29 -0600

    filters text inputs

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Text, View } from "./Themed"
+import React, { useState } from "react";
+import { TextInput } from "react-native";
+import { Text, View } from "./Themed";
 
-const Filters = (props :any) => {
+const Filters = (props: any) => {
+	const [lastName, setLastName] = useState("");
+	const [middleName, setMiddleName] = useState("");
 
-    return(
-        <View>
-            <Text>Filters</Text>
-        </View>
-    )
-}
+	return (
+		<View>
+			<Text>Filters</Text>
+			<View>
+				<TextInput
+					value={lastName}
+					placeholder="Last Name(optional)"
+					onChangeText={(text) => setLastName(text)}
+				/>
+			</View>
+			<View>
+				<TextInput
+					value={middleName}
+					placeholder="Middle Name(optional)"
+					onChangeText={(text) => setMiddleName(text)}
+				/>
+			</View>
+		</View>
+	);
+};
 
-export default Filters
+export default Filters;
