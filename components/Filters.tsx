c8ab1e98f902ebda3f04b4cb68653b1aa8041cfa
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-29 11:11:58 -0600

    dispatching filter actions

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 import { Button, StyleSheet, TextInput } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Text, View } from "./Themed";
+import * as filterActions from '../store/actions/filters'
 
 const Filters = (props: any) => {
     const filters = useSelector((state :any) => state.filters.filters)
 	const [lastName, setLastName] = useState(filters ? filters.lastName : "");
 	const [middleName, setMiddleName] = useState(filters ? filters.middleName : "");
-    
+    const dispatch = useDispatch()
 
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>Filters</Text>
-			<View>
+			<View style={styles.inputContainer}>
 				<TextInput
 					value={lastName}
 					placeholder="Last Name(optional)"
 					onChangeText={(text) => setLastName(text)}
 				/>
 			</View>
-			<View>
+			<View style={styles.inputContainer} >
 				<TextInput
 					value={middleName}
 					placeholder="Middle Name(optional)"
@@ -27,7 +28,7 @@ const Filters = (props: any) => {
 				/>
 			</View>
             <View style={styles.buttonContainer}>
-                <Button title='Save' onPress={() => {}}/>
+                <Button title='Save' onPress={() => {dispatch(filterActions.createFilter({lastName: lastName, middleName: middleName}))}}/>
                 <Button title='Clear' onPress={() => {}}/>
 
             </View>
@@ -37,6 +38,19 @@ const Filters = (props: any) => {
 
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: '#ddd'
+    },
+    inputContainer: {
+        width: '50%',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginVertical: 5,
+    },
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between'
