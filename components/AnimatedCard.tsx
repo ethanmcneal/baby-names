77a9d89aac6ddebde5e6c95bbfcc822c69729855
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-03 14:42:41 -0600

    changed how names are rendered

--- components/AnimatedCard.tsx
+++ components/AnimatedCard.tsx
@@ -1,24 +1,18 @@
-
 import React, { useEffect, useState } from "react";
-import {
-	Animated,
-	Dimensions,
-	StyleSheet,
-} from "react-native";
+import { Animated, Dimensions, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { Name } from "../types";
 import { Text, View } from "./Themed";
 
-
 const { width, height } = Dimensions.get("window");
 
+const AnimatedCard = (props: any) => {
+	const filters = useSelector((state: any) => state.filters.filters);
+	const { names, index, panResponder, position, gender } = props;
+	const name = names[index];
+	const nextName = names[index + 1];
 
-const AnimatedCard = (props :any) => {
-    
-    const filters = useSelector((state :any) => state.filters.filters)
-    const {names, index, panResponder, position, gender} = props
-
-    let rotate = position.x.interpolate({
+	let rotate = position.x.interpolate({
 		inputRange: [-width / 2, 0, width / 2],
 		outputRange: ["-8deg", "0deg", "8deg"],
 		extrapolate: "clamp",
@@ -42,69 +36,65 @@ const AnimatedCard = (props :any) => {
 			},
 			...position.getTranslateTransform(),
 		],
-    	};
+	};
+	return (
+		<>
+        <Animated.View
+            				style={[
+					{
+						opacity: nextCardOpacity,
+						transform: [{ scale: nextCardScale }],
+					},
+					gender === "girl" ? styles.girlCard : styles.boyCard,
+				]}
+			>
+				<Text style={styles.name}>
+					{nextName.name} {filters.middleName}
+					{filters.middleName && " "}
+					{filters.lastName}
+				</Text>
+			</Animated.View>
+			<Animated.View
+				{...panResponder.panHandlers}
+				// key={babyName.id}
+				style={
+					gender === "girl"
+						? [rotateAndTranslate, { ...styles.girlCard }]
+						: [rotateAndTranslate, { ...styles.boyCard }]
+				}
+			>
+				<Text style={styles.name}>
+					{name.name} {filters.middleName}
+					{filters.middleName && " "}
+					{filters.lastName}
+				</Text>
+			</Animated.View>
+			
+		</>
+    )}
 
-    return (names.map((babyName :Name, i: number) => {
-        if (i < index) {
-            return null;
-        } else if (i === index) {
-            return (
-                <Animated.View
-                    {...panResponder.panHandlers}
-                    key={babyName.id}
-                    style={gender === 'girl' ? 
-                       [ rotateAndTranslate,
-                        { ...styles.girlCard },
-                    ] : [
-                        rotateAndTranslate,
-                        { ...styles.boyCard },
-                    ]}
-                >
-                    <Text style={styles.name}>{babyName.name}{' '}{filters.middleName}{filters.middleName && ' '}{filters.lastName}</Text>
-                </Animated.View>
-            );
-        } else {
-            return (
-                <Animated.View
-                    key={babyName.id}
-                    style={[
-                        {
-                            opacity: nextCardOpacity,
-                            transform: [{ scale: nextCardScale }],
-                        },
-                        gender === 'girl' ? styles.girlCard : styles.boyCard,
-                    ]}
-                >
-                    <Text style={styles.name}>{babyName.name}{' '}{filters.middleName}{filters.middleName && ' '}{filters.lastName}</Text>
-                </Animated.View>
-            );
-        }
-    })
-    .reverse())
-}
+const card: any = {
+	height: 300,
+	alignItems: "center",
+	justifyContent: "center",
+	width: "100%",
+	position: "absolute",
+	top: "70%",
+	borderRadius: 12,
+};
 
-    const card :any = {
-        height: 300,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        position: "absolute",
-        top: "70%",
-        borderRadius: 12,
-}
+const styles = StyleSheet.create({
+	boyCard: {
+		...card,
+		backgroundColor: "#05BCEE",
+	},
+	girlCard: {
+		...card,
+		backgroundColor: "pink",
+	},
+	name: {
+		fontSize: 22,
+	},
+});
 
-    const styles = StyleSheet.create({
-        boyCard: {
-            ...card,
-            backgroundColor: "#05BCEE"
-        },
-        girlCard: {
-            ...card,
-            backgroundColor: 'pink'
-        },
-        name: {
-            fontSize: 22,
-        },
-    })
-
-    export default AnimatedCard
+export default AnimatedCard;
