cf7d068ed0d0e8c5222b124b507ecbba612e5c66
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-05 14:21:46 -0600

    styling on name cards

--- components/LikedName.tsx
+++ components/LikedName.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, View } from './Themed';
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { StyleSheet, useColorScheme } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
+import Colors from "../constants/Colors";
+import { Text, View } from "./Themed";
 
-const LikedName = (props :any) => {
-    return (
-        <View style={styles.nameRow}>
-                    <Text>{props.nameData.item.name}</Text>
-                    <Text>{props.nameData.item.id}</Text>
-        </View>
-    )
-}
+const LikedName = (props: any) => {
+	const filters = useSelector((state: any) => state.filters.filters);
+    const [showDetails, setShowDetails] = useState(false)
+	const color =
+		props.nameData.item.gender === "girl"
+			? Colors.universal.pink
+			: Colors.universal.blue;
+	return (
+        <>
+		<View style={styles.nameRow}>
+			<Text style={[{ ...styles.name }, { color }]}>
+				{props.nameData.item.name} {filters.middleName && " "}
+				{filters.lastName}
+			</Text>
+            <TouchableOpacity onPress={() => setShowDetails(!showDetails)}>
+                <Ionicons name={showDetails ? 'chevron-up' : 'chevron-down'} color={color} size={22}/>
+            </TouchableOpacity>
+		</View>
+        </>
+	);
+};
 
-export default LikedName
+export default LikedName;
 
 const styles = StyleSheet.create({
-    nameRow: {
-        flexDirection: 'row',
-        padding: 15,
-        justifyContent: 'space-between',
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-
-    }
-})
+	nameRow: {
+		flexDirection: "row",
+		padding: 15,
+		justifyContent: "space-between",
+		borderBottomWidth: 1,
+		borderBottomColor: "#ccc",
+	},
+	name: {
+		fontSize: 22,
+	},
+});
