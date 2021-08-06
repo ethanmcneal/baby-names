9ef56e25b3bc145fceac066a281e34719e1c68bd
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-06 16:09:05 -0600

    change country action

--- store/actions/name.ts
+++ store/actions/name.ts
@@ -2,6 +2,7 @@ import { Name } from "../../types";
 
 export const LIKE_NAME = 'LIKE_NAME'
 export const DISLIKE_NAME = 'DISLIKE_NAME'
+export const CHANGE_COUNTRY = 'CHANGE_COUNTRY'
 
 
 export const likeName = (name :Name) => {
@@ -10,4 +11,8 @@ export const likeName = (name :Name) => {
 
 export const dislikeName = (name :Name) => {
     return {type: DISLIKE_NAME, name: name};
+}
+
+export const changeCountry = (country :string) => {
+    return {type: CHANGE_COUNTRY, country: country}
 }
