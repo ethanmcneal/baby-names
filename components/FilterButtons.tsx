9726c84ccaa55961e9b5bf8b1b308e4dbb5acc3b
ethan.mcneal <ethan.mcneal@gmail.com>
2021-08-17 17:46:13 -0600

    button color

--- components/FilterButtons.tsx
+++ components/FilterButtons.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import React from "react";
+import { Button, StyleSheet, View } from "react-native";
+import Colors from "../constants/Colors";
 
-const FilterButtons = (props :any) => {
-
-    const {showDropdown, setShowDropdown, handleCountryChange, saveFilters} = props
-    return (
-        <View style={styles.buttonContainer}>
-				<Button title="Save" onPress={() => saveFilters()} />
-				{showDropdown && (
-					<Button
-						title="Cancel"
-						onPress={() => {
-							setShowDropdown(false);
-						}}
-					/>
-				)}
-				{!showDropdown && (
-					<Button
-						title="Change Country"
-						onPress={() => handleCountryChange()}
-					/>
-				)}
+const FilterButtons = (props: any) => {
+	const { showDropdown, setShowDropdown, handleCountryChange, saveFilters } =
+		props;
+	return (
+		<View style={styles.buttonContainer}>
+			<Button
+				title="Save"
+				onPress={() => saveFilters()}
+				color={Colors.universal.blue}
+			/>
+			{showDropdown && (
+				<Button
+					title="Cancel"
+					onPress={() => {
+						setShowDropdown(false);
+					}}
+					color={Colors.universal.blue}
+				/>
+			)}
+			{!showDropdown && (
+				<Button
+					title="Change Country"
+					onPress={() => handleCountryChange()}
+					color={Colors.universal.blue}
+				/>
+			)}
 		</View>
-        )
-}
-
+	);
+};
 
 const styles = StyleSheet.create({
-    buttonContainer: {
+	buttonContainer: {
 		flexDirection: "row",
 		justifyContent: "space-between",
 	},
-})
-export default FilterButtons
+});
+export default FilterButtons;
--- components/FilterInputs.tsx
+++ components/FilterInputs.tsx
@@ -5,71 +5,68 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { Text } from "./Themed";
 import Colors from "../constants/Colors";
 
-const FilterInputs = (props :any) => {
+const FilterInputs = (props: any) => {
+	const colorScheme = useColorScheme();
+	const { lastName, middleName, setLastName, setMiddleName } = props;
 
-   const colorScheme = useColorScheme()
-    const {lastName, middleName, setLastName, setMiddleName} = props
-
-
-    const inputText = {
+	const inputText = {
 		color: colorScheme === "dark" ? Colors.dark.text : Colors.light.text,
 		fontSize: 22,
 	};
-    return(
-        <>
-        <View style={styles.inputContainer}>
-        <TextInput
-            style={inputText}
-            value={lastName}
-            placeholder="Last Name(optional)"
-            onChangeText={(text) => setLastName(text)}
-        />
-        <Text>
-            {lastName && (
-                <TouchableOpacity onPress={() => setLastName("")}>
-                    <Text>
-                        <Ionicons
-                            name="ios-backspace-outline"
-                            color={inputText.color}
-                            size={25}
-                        />
-                    </Text>
-                </TouchableOpacity>
-            )}
-        </Text>
-    </View>
-    <View style={styles.inputContainer}>
-        <TextInput
-            style={inputText}
-            value={middleName}
-            placeholder="Middle Name(optional)"
-            onChangeText={(text) => setMiddleName(text)}
-        />
-        <Text>
-            {middleName && (
-                <TouchableOpacity
-                    onPress={() => {
-                        setMiddleName("");
-                    }}
-                >
-                    <Text>
-                        <Ionicons
-                            name="ios-backspace-outline"
-                            color={inputText.color}
-                            size={25}
-                        />
-                    </Text>
-                </TouchableOpacity>
-            )}
-        </Text>
-    </View>
-    </>
-
-    )
-}
+	return (
+		<>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={inputText}
+					value={lastName}
+					placeholder="Last Name(optional)"
+					onChangeText={(text) => setLastName(text)}
+				/>
+				<Text>
+					{lastName && (
+						<TouchableOpacity onPress={() => setLastName("")}>
+							<Text>
+								<Ionicons
+									name="ios-backspace-outline"
+									color={inputText.color}
+									size={25}
+								/>
+							</Text>
+						</TouchableOpacity>
+					)}
+				</Text>
+			</View>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={inputText}
+					value={middleName}
+					placeholder="Middle Name(optional)"
+					onChangeText={(text) => setMiddleName(text)}
+				/>
+				<Text>
+					{middleName && (
+						<TouchableOpacity
+							onPress={() => {
+								setMiddleName("");
+							}}
+						>
+							<Text>
+								<Ionicons
+									name="ios-backspace-outline"
+									color={inputText.color}
+									size={25}
+								/>
+							</Text>
+						</TouchableOpacity>
+					)}
+				</Text>
+			</View>
+		</>
+	);
+};
 
 const styles = StyleSheet.create({
-    inputContainer: {
+	inputContainer: {
 		width: "70%",
 		borderBottomColor: "#ccc",
 		borderBottomWidth: 1,
@@ -78,6 +75,6 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		alignItems: "center",
 	},
-})
+});
 
-export default FilterInputs
+export default FilterInputs;
