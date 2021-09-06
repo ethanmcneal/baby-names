d216878ad92f6a79d595d0abaf96240075d64bcd
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-06 13:46:05 -0600

    async storage function written

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { useState } from 'react'
 
 const LoadingScreen = (props :any) => {
     const {setLoading} = props
+    const [isDataLoaded, setIsDataLoaded] = useState(false)
+
+    const getAsyncStorageData = async() => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('@storage_Key')
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+          } catch(e) {
+            // error reading value
+          }
+    }
 }
