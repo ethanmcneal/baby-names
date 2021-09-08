f011ce9bc28a0e896adeed7431bb2b990d270459
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-08 14:35:34 -0600

    refactor

--- components/NamesComponents/NameCard.tsx
+++ components/NamesComponents/NameCard.tsx
@@ -6,49 +6,59 @@ import { View } from "../Themed";
 import * as nameActions from "../../store/actions/name";
 import GenderButtons from "./GenderButtons";
 import LikeButtons from "./LikeButtons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+	DISLIKED_NAMES_KEY,
+	LIKED_NAMES_KEY,
+} from "../../constants/StorageKeys";
 
 const NameCard = (props: any) => {
-
-	const {showFilters, nextID, index, setIndex, gender, setGender} = props
+	const { showFilters, nextID, index, setIndex, gender, setGender } = props;
 	const names = useSelector((state: any) => state.names.names);
-	
+
 	const dispatch = useDispatch();
 
 	const { width, height } = Dimensions.get("window");
 
-	const handleSwipe = (action: any) => {
+	const handleSwipe = async (action: any) => {
 		setTimeout(() => {
 			setIndex((prev: number) => prev + 1),
-			dispatch(
-				action(index, gender)
-			)
-		}, 90); //timeout set so the animation will play before the index is incremented, that way the card doesn't
-				// vanish instantly
-				//I want the timeout to be quick enough for users to spam the like button
-				//but slow enough so they can't get duplicates in likedNames
+				dispatch(action(index, gender));
+		}, 90); //timeout set so the animation will play before the index is incremented,
+		//that way the card doesn't vanish instantly
+		//I want the timeout to be quick enough for users to spam the like button
+		//but slow enough so they can't get duplicates in likedNames
+		try {
+			let res;
+			if (action === nameActions.likeName) {
+				res = await AsyncStorage.getItem(LIKED_NAMES_KEY);
+			} else {
+				res = await AsyncStorage.getItem(DISLIKED_NAMES_KEY);
+			}
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	let position: any = new Animated.ValueXY();
 	const onRelease = (evt: any, gestureState: any) => {
 		if (gestureState.dx > 120) {
 			handleSwipe(nameActions.likeName),
-
 				Animated.spring(position, {
 					toValue: { x: width + 100, y: gestureState.dy },
 					useNativeDriver: false,
-					bounciness: 15
+					bounciness: 15,
 				}).start(() => {
-
 					() => {
 						position.setValue({ x: 0, y: 0 });
 					};
 				});
 		} else if (gestureState.dx < -120) {
-				handleSwipe(nameActions.dislikeName),
+			handleSwipe(nameActions.dislikeName),
 				Animated.spring(position, {
 					toValue: { x: -width - 300, y: gestureState.dy },
 					useNativeDriver: false,
-					bounciness: 15
+					bounciness: 15,
 				}).start(() => {
 					() => {
 						position.setValue({ x: 0, y: 0 });
@@ -75,13 +85,15 @@ const NameCard = (props: any) => {
 
 	return (
 		<View style={styles.container}>
-			{showFilters && <GenderButtons
-				gender={gender}
-				setIndex={setIndex}
-				setGender={setGender}
-				nextID={nextID}
-			/>}
-			<LikeButtons onRelease={onRelease}/>
+			{showFilters && (
+				<GenderButtons
+					gender={gender}
+					setIndex={setIndex}
+					setGender={setGender}
+					nextID={nextID}
+				/>
+			)}
+			<LikeButtons onRelease={onRelease} />
 			{gender == "girl" ? (
 				<AnimatedCard
 					names={names.girlNames}
