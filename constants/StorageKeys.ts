3282359bccd26e08dcc12d504f497c30280fea54
ethan.mcneal <ethan.mcneal@gmail.com>
2021-09-08 13:12:01 -0600

    storage keys constant

---
+++ constants/StorageKeys.ts
@@ -0,0 +1,4 @@
+export const LIKED_NAMES_KEY = '@liked_Names'
+export const DISLIKED_NAMES_KEY = '@disliked_Names'
+export const LAST_INTERACTED_ID_KEY = '@last_Interacted_Id'
+export const PREVIOUS_ID_STATE_KEY = '@previous_Id_State'
